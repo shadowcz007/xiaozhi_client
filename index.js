ae8ca18e79aa7711fec608280871ed552690d39b
shadow <chizhiwei007@163.com>
2025-06-27 21:34:38 +0800

    优化

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
-import { DeviceActivator } from './src/device-activator.js';
+import { checkDeviceStatus } from './src/device-status.js';
 import { Client } from './src/client.js';
 
-const activator = new DeviceActivator();
-let deviceId = 'f2:68:cb:b2:c5:93';
+let deviceId = 'df:52:34:be:fa:38';
+async function main() {
 
+    let statusResponse = await checkDeviceStatus(deviceId);
+    console.log('statusResponse', statusResponse);
+    if (!statusResponse) {
+        //需要激活
+        console.error('需要激活');
+        return
+    }
 
-let statusResponse = await activator.checkDeviceStatus(deviceId);
-console.log('statusResponse', statusResponse);
+    const example = new Client(
+        statusResponse.websocket.url,
+        statusResponse.websocket.token,
+        deviceId,
+        statusResponse.mqtt.client_id);
 
-const example = new Client(
-    statusResponse.websocket.url,
-    statusResponse.websocket.token,
-    deviceId,
-    statusResponse.mqtt.client_id);
+    // 监听状态变化
+    example.onStateChanged = (state) => {
+        console.log('当前状态:', state);
+        // 根据状态更新UI
+    };
 
-// 监听状态变化
-example.onStateChanged = (state) => {
-    console.log('当前状态:', state);
-    // 根据状态更新UI
-};
+    // 直接发送文字消息
+    // await example.sendTextMessage('今天天气怎么样？');
 
-// 直接发送文字消息
-// await example.sendTextMessage('今天天气怎么样？');
+    // 开始语音聊天（会发送hi并开启自动录音循环）
+    await example.startVoiceChat();
 
-// 开始语音聊天（会发送hi并开启自动录音循环）
-await example.startVoiceChat();
+    // // 打断对话
+    // await example.interruptConversation();
 
-// // 打断对话
-// await example.interruptConversation();
+    // // 停止语音聊天
+    // await example.stopVoiceChat();
+}
 
-// // 停止语音聊天
-// await example.stopVoiceChat();
+main();
--- src/device-status.js
+++ src/device-status.js
@@ -1,9 +1,25 @@
 import crypto from 'crypto';
 
 const otaUrl = 'https://api.tenclass.net/xiaozhi/ota/';
-const clientId = crypto.randomUUID();
 
-export async function checkDeviceStatus(payload, headers) {
+export async function checkDeviceStatus(device_id) {
+
+    const clientId = crypto.randomUUID();
+    const data = {
+        "device_id": device_id,
+        "payload": {},
+        "headers": {
+            "Activation-Version": "2",
+            "Device-Id": device_id,
+            "Client-Id": clientId,
+            "Content-Type": "application/json",
+            "User-Agent": "mix/1.0.0"
+        }
+    }
+
+    const payload = data.payload;
+    const headers = data.headers;
+
     try {
         const response = await fetch(otaUrl, {
             method: 'POST',
@@ -15,38 +31,17 @@ export async function checkDeviceStatus(payload, headers) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
 
-        return await response.json();
+        const result = await response.json();
+        if (result.activation) {
+            //需要激活
+            return
+        } else {
+            //正常使用
+            return result
+        }
+
     } catch (error) {
         console.error('检查设备状态失败:', error.message);
         throw error;
     }
-}
-
-if (
-    import.meta.url ===
-    import.meta.resolve(process.argv[1])) {
-
-    const data = {
-        "device_id": "df:52:34:be:fa:38",
-        "payload": {},
-        "headers": {
-            "Activation-Version": "2",
-            "Device-Id": "df:52:34:be:fa:38",
-            "Client-Id": clientId,
-            "Content-Type": "application/json",
-            "User-Agent": "mix/1.0.0"
-        }
-    }
-
-    const payload = data.payload;
-    const headers = data.headers;
-
-    const result = await checkDeviceStatus(payload, headers);
-    if (result.activation) {
-        //需要激活
-    } else {
-        //正常使用
-
-    }
-    console.log('##result', result);
 }
