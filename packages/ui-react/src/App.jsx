89e53c1db1f1cd8efdeb001907149235ce2b57f6
shadow <chizhiwei007@163.com>
2025-06-29 07:23:53 +0800

    fx

--- packages/ui-react/src/App.jsx
+++ packages/ui-react/src/App.jsx
@@ -1,1 +1,12 @@
- 
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>XiaoZhi UI</h1>
+    </div>
+  );
+}
+
+export default App; 
